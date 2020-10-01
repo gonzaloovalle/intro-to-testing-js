0b686162946194640ada81b4939a49e71ec504aa
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2020-10-01 14:45:30 -0500

    added tests

--- tests.js
+++ tests.js
@@ -18,6 +18,15 @@ describe('helloWorld', function() {
 //unit test for sayHello function
 describe('sayHello', function(){
     it('should be a defined function', function(){
-       expect(typeof sayHello).toBe('function')
+        expect(typeof sayHello).toBe('function')
+    });
+    it('should return a string when called', function(){
+        expect(typeof sayHello()).toBe('string');
+    });
+    it('should return the string "Say Hello!" when executed', function(){
+        expect(sayHello()).toBe("Say Hello!");
+    });
+    it("should never return 'undefined' when called", function(){
+        expect(sayHello()).not.toBe(undefined);
     });
 });
